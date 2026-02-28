58f2e873447b4f4f5719f95393085eeed31bc4a3
Azat Ruslanov <aruslanov869@gmail.com>
2026-03-01 03:15:46 +0600

    Maybe fixedddd

--- services/instagram.py
+++ services/instagram.py
@@ -1,11 +1,11 @@
-import requests
+# import requests
 
-def send_instagram_message(recipient_id, text):
-    url = f"https://graph.facebook.com/v19.0/me/messages?access_token={INSTAGRAM_TOKEN}"
+# def send_instagram_message(recipient_id, text):
+#     url = f"https://graph.facebook.com/v19.0/me/messages?access_token={INSTAGRAM_TOKEN}"
 
-    payload = {
-        "recipient": {"id": recipient_id},
-        "message": {"text": text}
-    }
+#     payload = {
+#         "recipient": {"id": recipient_id},
+#         "message": {"text": text}
+#     }
 
-    requests.post(url, json=payload)
+#     requests.post(url, json=payload)

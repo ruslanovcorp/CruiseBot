073c8e59036d34445a85b4f73aa7449503290fee
Azat Ruslanov <aruslanov869@gmail.com>
2026-03-01 20:39:31 +0600

    added frontend

--- app/main.py
+++ app/main.py
@@ -6,6 +6,17 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "https://cruise-admin.vercel.app",  # потом пригодится
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(webhook.router)
 app.include_router(admin.router)
 app.include_router(auth.router)
